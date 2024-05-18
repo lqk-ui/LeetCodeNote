6e7a2b6d713048a896934da7bfd81f8e682eaf4a
lqk-ui <67775857+lqk-ui@users.noreply.github.com>
2024-05-18 16:06:33 +0800

    Create #1.两数之和.java

---
+++ 哈希表/#1.两数之和.java
@@ -0,0 +1,21 @@
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+你可以按任意顺序返回答案。
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            int x = target - nums[i];
+            if (hashtable.containsKey(x)) {
+                return new int[] { hashtable.get(x), i };
+            } else {
+                hashtable.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
